ebab8f4b87793a216eacb55ba894351faf49e986
miller barahona <millermiller01@hotmail.com>
2020-08-09 17:39:05 -0500

    Primer Hola Mundo

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package holamundo;
 
 /**
@@ -15,7 +11,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
